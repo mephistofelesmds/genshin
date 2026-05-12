649183c1d31b15e30f66742932eb0f10d665d286
mephistofefaust <arinatarasenko17@gmail.com>
2026-05-12 19:07:02 +0300

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,13 +1,13 @@
 // Все 5★ персонажи
 const ALL_FIVE_STARS = [
-    { id: '5-1', name: 'Архонт Огня', rarity: 5 },
-    { id: '5-2', name: 'Архонт Воды', rarity: 5 },
-    { id: '5-3', name: 'Архонт Воздуха', rarity: 5 },
-    { id: '5-4', name: 'Архонт Земли', rarity: 5 },
-    { id: '5-5', name: 'Архонт Молнии', rarity: 5 },
-    { id: '5-6', name: 'Архонт Льда', rarity: 5 },
-    { id: '5-7', name: 'Архонт Тьмы', rarity: 5 },
-    { id: '5-8', name: 'Архонт Света', rarity: 5 },
+    { id: '5-1', name: 'Мефисто', rarity: 5 },
+    { id: '5-2', name: 'Фици', rarity: 5 },
+    { id: '5-3', name: 'Молли', rarity: 5 },
+    { id: '5-4', name: 'Лишайник', rarity: 5 },
+    { id: '5-5', name: 'Шино', rarity: 5 },
+    { id: '5-6', name: 'Шанон', rarity: 5 },
+    { id: '5-7', name: 'Мису', rarity: 5 },
+    { id: '5-8', name: 'Лихуа', rarity: 5 },
 ];
 
 // Все 4★ персонажи
@@ -39,59 +39,59 @@ const HALF_2 = ALL_FIVE_STARS.slice(4, 8); // Молния, Лёд, Тьма, С
 const BANNERS = [
     {
         id: 1,
-        name: 'Баннер Огня',
+        name: 'Электро',
         fiveStar: HALF_1[0],        // Архонт Огня
         fourStars: [ALL_FOUR_STARS[0], ALL_FOUR_STARS[1], ALL_FOUR_STARS[2]], // Рыцарь, Лучница, Маг
-        color: '#ff4500'
+        color: '#9370db'
     },
     {
         id: 2,
-        name: 'Баннер Воды',
+        name: 'Гидро',
         fiveStar: HALF_1[1],        // Архонт Воды
         fourStars: [ALL_FOUR_STARS[2], ALL_FOUR_STARS[3], ALL_FOUR_STARS[4]], // Маг, Ассасин, Бард
         color: '#00bfff'
     },
     {
         id: 3,
-        name: 'Баннер Воздуха',
+        name: 'Анэмо',
         fiveStar: HALF_1[2],        // Архонт Воздуха
         fourStars: [ALL_FOUR_STARS[4], ALL_FOUR_STARS[5], ALL_FOUR_STARS[6]], // Бард, Кузнец, Жрица
-        color: '#7fff00'
+        color: '#87ceeb'
     },
     {
         id: 4,
-        name: 'Баннер Земли',
+        name: 'Дендро',
         fiveStar: HALF_1[3],        // Архонт Земли
         fourStars: [ALL_FOUR_STARS[6], ALL_FOUR_STARS[7], ALL_FOUR_STARS[0]], // Жрица, Странник, Рыцарь
-        color: '#daa520'
+        color: '#7fff00'
     },
     {
         id: 5,
-        name: 'Баннер Молнии',
+        name: 'Пиро',
         fiveStar: HALF_2[0],        // Архонт Молнии
         fourStars: [ALL_FOUR_STARS[1], ALL_FOUR_STARS[3], ALL_FOUR_STARS[5]], // Лучница, Ассасин, Кузнец
-        color: '#9370db'
+        color: '#ff4500'
     },
     {
         id: 6,
-        name: 'Баннер Льда',
-        fiveStar: HALF_2[1],        // Архонт Льда
+        name: 'Крио',
+        fiveStar: HALF_2[1],
         fourStars: [ALL_FOUR_STARS[0], ALL_FOUR_STARS[4], ALL_FOUR_STARS[7]], // Рыцарь, Бард, Странник
         color: '#87ceeb'
     },
     {
         id: 7,
-        name: 'Баннер Тьмы',
-        fiveStar: HALF_2[2],        // Архонт Тьмы
+        name: 'Гео',
+        fiveStar: HALF_2[2],      
         fourStars: [ALL_FOUR_STARS[2], ALL_FOUR_STARS[5], ALL_FOUR_STARS[6]], // Маг, Кузнец, Жрица
-        color: '#4b0082'
+        color: '#daa520'
     },
     {
         id: 8,
-        name: 'Баннер Света',
-        fiveStar: HALF_2[3],        // Архонт Света
+        name: 'Аура',
+        fiveStar: HALF_2[3],       
         fourStars: [ALL_FOUR_STARS[3], ALL_FOUR_STARS[6], ALL_FOUR_STARS[7]], // Ассасин, Жрица, Странник
-        color: '#ffd700'
+        color: '#ffccff'
     }
 ];
 
